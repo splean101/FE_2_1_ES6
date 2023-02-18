c5e08ccc15dcfcfad4f1126185284ac55f637acd
splean101 <101splean@gmail.com>
2023-02-18 11:52:08 +0200

    2 done

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// -----------1-------------
+
+const usersNames = ['Mike', 'Bob', 'Nilola'];
+const users = {};
+[users.Mike, users.Bob, users.Nicola] = usersNames;
+console.log(users);
+
+// -----------2-------------
+
+const salaries = {
+  Mike: 1500,
+  Bob: 1800,
+  Nicola: 1700,
+};
+
+function maxSalary(salaries) {
+  let maxSalary = 0;
+  let employeeName = '';
+  for (const [name, salary] of Object.entries(salaries)) {
+    if (salary > maxSalary) {
+      maxSalary = salary;
+      employeeName = name;
+    }
+  }
+  return employeeName;
+}
+
+console.log(maxSalary(salaries));
