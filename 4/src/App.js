5ca6d27c9230c06abb6d3cc80adb68aea221dd09
splean101 <101splean@gmail.com>
2023-02-18 14:43:39 +0200

    done

--- 4/src/App.js
+++ 4/src/App.js
@@ -1,14 +1,31 @@
-import Hello from './Hello.js';
+// import Hello from './Hello.js';
 
+// function App() {
+//   return (
+//     <div style={{ backgroundColor: '#46B1DE' }} className="wrapper">
+//       <Hello />
+//       <button onClick={() => console.log('Click on button')}>Button</button>
+//     </div>
+//   );
+// }
+
+// const domContainer = document.querySelector('#root');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(App);
+
+import Hello from './Hello.js';
 function App() {
   return (
-    <div style={{ backgroundColor: '#46B1DE' }} className="wrapper">
-      <Hello />
-      <button onClick={() => console.log('Click on button')}>Button</button>
-    </div>
+    //<div style={{ backgroundColor: '#46B1DE' }} className="wrapper">
+      <Hello></Hello>
+      //<button onClick={() => console.log('Click on button')}>Button</button>
+//</div>
   );
 }
-
 const domContainer = document.querySelector('#root');
 const root = ReactDOM.createRoot(domContainer);
-root.render(App);
+root.render(<App />);
+function Hello() {
+  return <h2>Hello React</h2>;
+}
+export default Hello;
